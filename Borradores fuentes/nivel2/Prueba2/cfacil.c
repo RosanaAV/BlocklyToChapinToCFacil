6cc0b969053d986d0015cd9bb69bd5935cd85364
RosanaAV <rav0011@alu.ubu.es>
2017-09-08 13:48:43 +0200

    #9 Creación de "nivel2"
    Se crea la carpeta "nivel2" en "Borradores fuentes" donde se amplia "practica.jj" para poder asignar valores a una variable y pruebas

---
+++ Borradores fuentes/nivel2/Prueba2/cfacil.c
@@ -0,0 +1,13 @@
+#define PROGRAMA "Nivel 2"
+#define DESCRIPCION "Programa generado automaticamente para TFG_BlocklyToChapinToCFacil"
+#define AUTOR "Rosana Arnaiz Vicario"
+#define VERSION  "v1.0"
+
+#include "c_facil.h"
+
+
+PRG(){
+escribeNumero(x);
+escribeFrase("Hola mundo");
+
+}
---
+++ Borradores fuentes/nivel2/Prueba3/cfacil.c
@@ -0,0 +1,13 @@
+#define PROGRAMA "Nivel 2"
+#define DESCRIPCION "Programa generado automaticamente para TFG_BlocklyToChapinToCFacil"
+#define AUTOR "Rosana Arnaiz Vicario"
+#define VERSION  "v1.0"
+
+#include "c_facil.h"
+
+
+PRG(){
+asignaValorA(var, pideNumero("Introduce un número"));
+escribeNumero(var);
+
+}
