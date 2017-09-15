3f6e50af4817868dabd9c7def7aa1074339e5f05
RosanaAV <rav0011@alu.ubu.es>
2017-09-15 12:43:31 +0200

    #10 Nivel 2
    La aplicación asigna valores a variables correctamente.
    Además se crea la carpeta "nivel3" donde se va a ampliar la aplicación con el fin de poder crear funciones

---
+++ Borradores fuentes/nivel3/Prueba3/cfacil.c
@@ -0,0 +1,17 @@
+//#define PROGRAMA "Nivel 3"
+//#define DESCRIPCION "Programa del nivel 3"
+//#define AUTOR "Programa generado automaticamente para TFG_BlocklyToChapinToCFacil"
+//#define VERSION  "v1.0"
+
+#include "c_facil.h"
+
+
+/*Función que muestra por pantalla el texto "¡Hola!"*/
+PROCEDIMIENTO saludo(texto) {
+escribeNumero(texto);
+}
+
+PRG(){
+saludo("¡Hola!");
+
+}
---
+++ Borradores fuentes/nivel3/Pruebas anteriores/Prueba2-1/cfacil.c
@@ -0,0 +1,12 @@
+//#define PROGRAMA "Nivel 3"
+//#define DESCRIPCION "Programa del nivel 3"
+//#define AUTOR "Programa generado automaticamente para TFG_BlocklyToChapinToCFacil"
+//#define VERSION  "v1.0"
+
+#include "c_facil.h"
+
+
+PRG(){
+escribeNumero(0escribeFrase("Prueba con un número");
+
+}
