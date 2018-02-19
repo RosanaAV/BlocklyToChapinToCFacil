7a3b54a736d6f65243aa2d49a0be7ee6142c2e42
rav0011 <rav0011@alu.ubu.es>
2018-02-19 23:59:44 +0100

    Repositorio

--- Pruebas/Prueba4/prueba4.c
+++ Pruebas/Prueba4/prueba4.c
@@ -6,4 +6,36 @@
 #include "c_facil.h"
 
 
+/*Función que calcula el perímetro de un rectángulo
+Entradas: base, altura
+    
+*/
+RACIONAL perimetro(RACIONAL x,RACIONAL y) {
+RACIONAL per; 
+asignaValorA(per, productoNumeros(sumaNumeros(x,y),2));
+
+return per;
+}
+
+/*Función que calcula el ñarea de un rectángulo
+Entradas: base, altura
+    
+*/
+RACIONAL area(RACIONAL x,RACIONAL y) {
+RACIONAL ar; 
+asignaValorA(ar, productoNumeros(x,y));
+
+return ar;
+}
+
 PRG(){
+RACIONAL b,h; 
+escribeFrase("Programa que calcula el perímetro y el área de un rectángulo");
+asignaValorA(b, pideNumero("Introduzca la base"));
+asignaValorA(h, pideNumero("Introduzca la altura"));
+escribeFrase("El perímetro del rectángulo es ");
+escribeNumero(perimetro(b,h));
+escribeFrase(" y su área es ");
+escribeNumero(area(b,h));
+
+}
